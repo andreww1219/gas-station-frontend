0b63ead54680e640d2e1c4a0ac13ee14690946d2
AndrewWang1219 <138897021+AndrewWang1219@users.noreply.github.com>
2024-01-04 11:10:47 +0800

    fix:login跳转

--- src/views/auth/LoginPage.vue
+++ src/views/auth/LoginPage.vue
@@ -31,7 +31,7 @@ const loginStore = useLoginStore()
         </RouterLink>
       </el-form>
       <div class="formBottom">
-        <el-button>登录</el-button>
+        <el-button @click="$router.push('/work-space')">登录</el-button>
         <div class="register-link" @click="$router.replace('/auth/register')">
           还没有账号? 注册
         </div>
@@ -62,7 +62,7 @@ const loginStore = useLoginStore()
   border-color: rgb(157,186,220);
 }
 .form-container {
-  width: 85%; 
-  margin: 0 auto; 
+  width: 85%;
+  margin: 0 auto;
 }
 </style>
